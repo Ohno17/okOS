c10e437c2ab1a5fd0d1ca66ca8939f51c1c9de7f
Ohno <107664053+Ohno17@users.noreply.github.com>
2022-06-18 03:22:55 +0000

    simple changes

--- Source/Kernel.cpp
+++ Source/Kernel.cpp
@@ -5,8 +5,5 @@ extern "C" void _start(){
 
 	SetCursorPosition(PositionFromCoords(0, 0));
 	PrintString("Hello World\n\rThis is a New Line!\n\r");
-	PrintString(HexToString(0x1234abcd));
-	PrintString("\n\r");
-	PrintString(HexToString("HI"));
 	return;	
 }
